dde6f298630354126a859adf8ba463cc98dfc9d3
Philip Greenwald <philip@avidest.com>
2016-01-18 19:26:47 -0500

    Added @document instance variable in mailer

--- app/mailers/transactions_mailer.rb
+++ app/mailers/transactions_mailer.rb
@@ -2,6 +2,7 @@ class TransactionsMailer < ActionMailer::Base
   default from: "Market Vault Downloads <downloads@marketvault.com>"
 
   def send_to_unsigned_in_user(email, document)
+  	@document = document
   	mail(to: email, subject: "Download #{document.title}").deliver
   end
 
