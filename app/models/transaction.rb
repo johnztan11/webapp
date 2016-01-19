8168cd65cdba39533148af443c3a228d28b469e3
Philip Greenwald <philip@avidest.com>
2016-01-18 19:11:29 -0500

    removed @self and comments from transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,16 +3,16 @@ class Transaction < ActiveRecord::Base
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
   belongs_to :author
 
-  # after_create :send_to_user
+  after_create :send_to_user
 
-  # def send_to_user
-  # 	puts "in send_to_user method".green
-  # 	puts "self before if is #{self.inspect}".green
-  # 	if self.non_user_email
-  # 		puts "self is #{@self.inspect}".blue
-  # 		@document = self.document
-  # 		@email = self.non_user_email
-  # 		TransactionsMailer.send_to_unsigned_in_user(@email, @document)
-  # 	end
-  # end
+  def send_to_user
+  	puts "in send_to_user method".green
+  	puts "self before if is #{self.inspect}".green
+  	if self.non_user_email
+  		puts "self is #{self.inspect}".blue
+  		@document = self.document
+  		@email = self.non_user_email
+  		TransactionsMailer.send_to_unsigned_in_user(@email, @document)
+  	end
+  end
 end
