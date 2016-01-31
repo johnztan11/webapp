301cb6223b904a6bd214d42b36b38ab4ebcd500e
zjohn9379 <zjohn9379@gmail.com>
2016-01-31 22:30:52 +0000

    fix-transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,17 +3,17 @@ class Transaction < ActiveRecord::Base
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
   belongs_to :author
 
-  after_create :send_to_user
+  # after_create :send_to_user
 
-  def send_to_user
-  	puts "in send_to_user method".green
-  	puts "self before if is #{self.inspect}".green
-  	if self.non_user_email
-  		puts "self is #{self.inspect}".blue
-      puts "document is #{self.document}".green
-  		@document = Document.find(self.document_id)
-  		@email = self.non_user_email
-  		TransactionsMailer.send_to_unsigned_in_user(@email, @document)
-  	end
-  end
+  # def send_to_user
+  # 	puts "in send_to_user method".green
+  # 	puts "self before if is #{self.inspect}".green
+  # 	if self.non_user_email
+  # 		puts "self is #{self.inspect}".blue
+  #     puts "document is #{self.document}".green
+  # 		@document = Document.find(self.document_id)
+  # 		@email = self.non_user_email
+  # 		TransactionsMailer.send_to_unsigned_in_user(@email, @document)
+  # 	end
+  # end
 end
