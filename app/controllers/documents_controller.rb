cedd8ee46aae55435b46e9971a17a4c9703e9bdc
zjohn9379 <zjohn9379@gmail.com>
2016-01-31 19:49:14 +0000

    make-font-white

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,18 +2,13 @@ class DocumentsController < ApplicationController
 
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
-
   before_action :set_document, only: [:show, :edit, :update, :destroy]
-
   before_action :redirect_if_not_author, only: [:new, :create, :edit, :update, :destroy]
-  # GET /documents
-  # GET /documents.json
+
   def index
     @documents = Document.all.includes(:author)
   end
 
-  # GET /documents/1
-  # GET /documents/1.json
   def show
     @document = Document.find(params[:id])
     @author = @document.author
@@ -23,21 +18,16 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # GET /documents/new
   def new
     @document = Document.new
   end
 
-  # GET /documents/1/edit
   def edit
   end
 
-  # POST /documents
-  # POST /documents.json
   def create
     @document = Document.new(document_params)
     @document.author = current_user
-
     respond_to do |format|
       if @document.save
         format.html { redirect_to documents_path, notice: 'Document was successfully created.' }
@@ -49,8 +39,6 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /documents/1
-  # PATCH/PUT /documents/1.json
   def update
     respond_to do |format|
       if @document.update(document_params)
@@ -63,8 +51,6 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # DELETE /documents/1
-  # DELETE /documents/1.json
   def destroy
     @document.destroy
     respond_to do |format|
@@ -86,8 +72,8 @@ class DocumentsController < ApplicationController
 
     def redirect_if_not_author
       unless current_user.author?
-        flash[:error] = "You need to register as an author to upload a document.  Fill out the info below to get started!"
-        redirect_to edit_author_path(current_user) 
+        flash[:error] = "You need to register as an author to upload a document. Please register with us."
+        redirect_to "/"
       end
     end
-end
+  end
