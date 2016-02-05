2c4796fdb26f10b82dd8a28e33afdb98ad182547
zjohn9379 <zjohn9379@gmail.com>
2016-02-05 03:21:35 +0000

    test-fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'landings/index'
+#  get 'landings/index'
 
   resources :documents
   resources :transactions, only: [:create]
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
   devise_scope :user do
     root to: 'users/registrations#new'
   end
